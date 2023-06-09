65fe3ce061235b0942dedff4cc3f541c49eb81c4
th50989 <th50989@gmail.com>
2023-06-09 09:48:00 +0700

    finish note page

--- App.js
+++ App.js
@@ -9,31 +9,31 @@ import Quotes from './Component/Quotes';
 import Dictionary from './Component/Dictionary';
 import { createStackNavigator } from '@react-navigation/stack';
 import Math from './Component/Math';
+import Note from './Component/Note';
 const Stack = createStackNavigator();
 
 function StackNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="LearnSomeThing"   options={{ headerShown: false }} component={HomePage}></Stack.Screen>
-      <Stack.Screen name="Math"  options={{ headerShown: false }} component={Math}></Stack.Screen>
+      <Stack.Screen name="LearnSomeThing" options={{ headerShown: false }} component={HomePage}></Stack.Screen>
+      <Stack.Screen name="Math" options={{ headerShown: false }} component={Math}></Stack.Screen>
     </Stack.Navigator>
   );
 }
 const Drawer = createDrawerNavigator();
-class App extends Component{
-  render()
-  {
+class App extends Component {
+  render() {
     return (
       <NavigationContainer>
         <Drawer.Navigator>
-        <Drawer.Screen name="Calculator" component={Calculator} />
-          <Drawer.Screen name="Home"  component={StackNavigator} />
-         
+          <Drawer.Screen name ="Note" component={Note} />
+          <Drawer.Screen name="Home" component={StackNavigator} />
+          <Drawer.Screen name="Calculator" component={Calculator} />
           <Drawer.Screen name="Quotes" component={Quotes} />
           <Drawer.Screen name="Dictionary" component={Dictionary} />
         </Drawer.Navigator>
       </NavigationContainer>
-      
+
     );
   }
 }
---
+++ Component/Note.js
@@ -0,0 +1,153 @@
+import { useState, useCallback } from "react";
+import { Text, View, StyleSheet, ScrollView, Button, Modal, TextInput, TouchableOpacity } from "react-native";
+
+
+
+const Note = () => {
+    const [notes, setNotes] = useState([])
+    const [listView, setListView] = useState(false)
+    const [isNoteModalOpen, setNoteModalOpen] = useState(false)
+    const [text, setText] = useState('')
+    const [SelectedIndex,setSelectedIndex] = useState(null)
+
+
+
+    const onNotePress = (index) => {
+        setSelectedIndex(index)
+        setText(notes[index])
+        setNoteModalOpen(true)
+    }
+    const onSaveNote = useCallback(() => {
+        if (!text) {
+            return;
+        }
+        const copyNotes = [...notes]
+        if (SelectedIndex != null)
+        {
+            copyNotes[SelectedIndex]= text
+        }
+        else copyNotes=copyNotes.concat(text)
+
+
+
+        setNotes(copyNotes)
+        setNoteModalOpen(false)
+        setSelectedIndex(null)
+    }, [text, notes, setNotes])
+
+
+    let noteStyle = [styles.note]
+    const textProps = listView ? { numberOfLines: 3 } : {}
+    if (listView) {
+        noteStyle = noteStyle.concat(styles.noteListView)
+    }
+
+    return (
+        <View style={{flex:1}}>
+            <View style={styles.buttonChoice}>
+                <Button title={listView ? "FullView" : "List View"} onPress={() => setListView(!listView)}></Button>
+            </View>
+
+            <ScrollView>
+                <View style={styles.noteContainer}>
+                    {
+                        notes.map(
+                            (note, index) => (
+                               <TouchableOpacity key={index} onPress={() => onNotePress(index)}>
+                                    <View  style={noteStyle}>
+                                        <Text {...textProps}>{note}</Text>
+                                    </View>
+                               </TouchableOpacity>
+                            )
+                        )
+                    }
+
+
+
+                </View>
+                <View style={{ padding: 10, alignItems: 'flex-end' }}>
+                    <Button style={[{ justifyContent: 'flex-end', alignItems: 'flex-end' }]} title="ADD NOTE" onPress={() => {
+                        setText('')
+                        setNoteModalOpen(true)
+                    }}></Button>
+                </View>
+            </ScrollView>
+
+            <Modal visible={isNoteModalOpen}>
+                <View style={styles.modalHeadingContainer}>
+                    <Text style={styles.modalHeading}>Add a note here</Text>
+                </View>
+                <View>
+                    <TextInput onChangeText={setText} value={text} multiline style={{borderWidth:1}} >
+
+                    </TextInput>
+                </View>
+                <View style={[styles.actionbuttonContainer]}>
+                    <View style={styles.modalButton}>
+                        <Button title='Save' onPress={() => onSaveNote()}></Button>
+                    </View>
+
+                    <View style={styles.modalButton}>
+                        <Button color='red' title='Cancel' onPress={() => setNoteModalOpen(false)}></Button>
+                    </View>
+
+                </View>
+            </Modal>
+
+        </View>
+
+    );
+}
+
+
+const styles = StyleSheet.create({
+    fab: {
+        alignItems: 'flex-end'
+        // Add any necessary styles for your app content
+    },
+    // container:{
+    //     justifyContent:'flex-end'
+    // }
+
+
+    noteContainer: {
+        padding: 10,
+        paddingRight: 20,
+
+    },
+    note: {
+        borderWidth: 2,
+        borderColor: '#ddd',
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+    noteListView: {
+        height: 70
+    },
+    buttonChoice: {
+        alignItems: 'flex-end',
+        padding: 10,
+
+    },
+    addNoteContainer: {
+
+    },
+    actionbuttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    modalButton: {
+        flex: 1,
+        padding: 10,
+        paddingTop:25
+    },
+    modalHeading: {
+        fontSize: 30,
+        fontWeight: "bold"
+    },
+    modalHeadingContainer: {
+        alignItems: 'center'
+    }
+});
+
+export default Note;
